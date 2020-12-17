7d3c0f9a01c6a79c0456d4cca72f1b8e2790325f
jmgtheworld <avedj94@hotmail.com>
2020-12-17 13:40:44 -0500

    add firevent for selecting interviewer for testing attempting to save after trying to submit an empty student name

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -43,10 +43,10 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it.only("can successfully save after trying to submit an empty student name", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } = render(
+      <Form interviewers={interviewers}  onSave={onSave} />
     );
   
     fireEvent.click(getByText("Save"));
@@ -57,9 +57,14 @@ describe("Form", () => {
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
       target: { value: "Lydia Miller-Jones" }
     });
-  
+
+    fireEvent.click(getByAltText("Sylvia Palmer"))
+
     fireEvent.click(getByText("Save"));
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
 
   });
 
